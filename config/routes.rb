47b774eabada7f09358f2f10135e697b521f8077
Eric Wee <wee007@gmail.com>
2018-05-15 01:10:57 +1000

    Define page view route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/page_view', to: 'page_view#index'
+  root to: 'page_view#index'
 end
