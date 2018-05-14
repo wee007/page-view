3123ff1df2cf90865cc9a03225483763d7fe19ab
Eric Wee <wee007@gmail.com>
2018-05-15 01:39:20 +1000

    Create react form

--- app/javascript/components/PageView.js
+++ app/javascript/components/PageView.js
@@ -2,19 +2,50 @@ import React from "react"
 import PropTypes from "prop-types"
 
 class PageView extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      data: [],
+      url: "",
+      before: "",
+      after: "",
+      interval: ""
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    const target = event.target
+    this.setState({
+      [target.name]: target.value
+    })
+  }
+
+  handleSubmit(event) {
+    alert("Woot!")
+    event.preventDefault()
+  }
+
   render () {
     return (
       <React.Fragment>
-        Url: {this.props.url}
-        Before: {this.props.before}
-        After: {this.props.after}
-        Interval: {this.props.interval}
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            URLs:
+            <input type="text" name="url" value={this.state.url} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
       </React.Fragment>
     )
   }
 }
 
 PageView.propTypes = {
+  data: PropTypes.array,
   url: PropTypes.string,
   before: PropTypes.string,
   after: PropTypes.string,
